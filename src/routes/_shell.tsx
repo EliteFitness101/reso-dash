a5c367b5a1270131b305a96a11504c02e3d62556
EliteFitness101 <resonancefitnessng@gmail.com>
2026-08-18 02:02:52 +0100

    Add XP and Tier 2 progression to member dashboard

--- src/routes/_shell.tsx
+++ src/routes/_shell.tsx
@@ -4,6 +4,7 @@ import { BottomTabBar } from "@/components/BottomTabBar";
 import { InstallPrompt } from "@/components/InstallPrompt";
 import { GridBackdrop } from "@/components/GridBackdrop";
 import { AdminPanel } from "@/components/admin/AdminPanel";
+import { XPProgress } from "@/components/XPProgress";
 
 export const Route = createFileRoute("/_shell")({
   component: ShellLayout,
@@ -16,7 +17,10 @@ function ShellLayout() {
       <AppHeader />
       <main className="relative z-10 mx-auto w-full max-w-md px-4 pb-44 pt-4">
         <AdminPanel />
-        <Outlet />
+        <XPProgress />
+        <div className="mt-4">
+          <Outlet />
+        </div>
       </main>
       <InstallPrompt />
       <BottomTabBar />
